5d34ea9c8e3ac7fdaa862f64ec3415db6615a754
jlasai <jlasauriarte@gmail.com>
2024-12-13 11:42:59 +0100

    first commit

---
+++ jokoa/Inicio/inicio.js
@@ -0,0 +1,9 @@
+// Función para redirigir a la siguiente página
+function playGame() {
+    // Reemplaza 'siguientePagina.html' con la URL de tu próxima página HTML
+    window.location.href = 'siguientePagina.html';
+}
+function exit() {
+    // Reemplaza 'siguientePagina.html' con la URL de tu próxima página HTML
+    window.location.href = 'siguientePagina.html';
+}
