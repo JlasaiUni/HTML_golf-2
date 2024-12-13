b1e2d16c20238b381cd3cd81ff594a722abdb06b
jlasai <jlasauriarte@gmail.com>
2024-12-13 12:48:44 +0100

    he cambiado basicamente todo :)

--- jokoa/Inicio/inicio.js
+++ jokoa/Inicio/inicio.js
@@ -1,7 +1,7 @@
 // Función para redirigir a la siguiente página
 function playGame() {
     // Reemplaza 'siguientePagina.html' con la URL de tu próxima página HTML
-    window.location.href = 'siguientePagina.html';
+    window.location.href = "nibelak/1nibela.html";
 }
 function exit() {
     // Reemplaza 'siguientePagina.html' con la URL de tu próxima página HTML
